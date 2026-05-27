c9d9529c1dddc0dd06ed041fe3c6c6c5b54b4e21
kite <jiaolina8123@outlook.com>
2026-05-27 22:23:09 +0800

    feat: 添加线上预览链接，使用ngrok内网穿透

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 5173
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['macaroni-snuff-clergyman.ngrok-free.dev']
   }
 });
